acad68e2c10ecfce1c1fe9765573799f3b5d7771
Zach Frew <zach.frew@wwt.com>
2021-06-15 14:04:53 -0500

    Initial Commit

---
+++ TestingAsyncAwait/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingAsyncAwait
+//
+//  Created by Zach Frew on 6/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestingAsyncAwait/TestingAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAsyncAwaitApp.swift
+//  TestingAsyncAwait
+//
+//  Created by Zach Frew on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct TestingAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
